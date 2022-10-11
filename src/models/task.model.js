e9ce6693edeaa3c94ebab08ba825e51d262833b9
deepcodeboy <ayrtonwe13@gmail.com>
2022-10-11 19:00:05 -0300

    Creacion de modelo de tareas

---
+++ src/models/task.model.js
@@ -0,0 +1,37 @@
+const {model, Schema} = require('mongoose');
+require('../models/user.model');
+
+const taskModel = new Schema({
+    title:{
+        type: 'string',
+        max: 100,
+        required: true
+    },
+    description:{
+        type:'string',
+        max: 255,
+        required: true
+    },
+    status:{
+        type:'string',
+        enum:['pending', ' in progress', 'complete', 'uncompleted'],
+        default: 'pending'
+    },
+    isActive:{
+        type: 'boolean',
+        default: true
+    },
+    idUser:{
+        type: Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+},{
+    timestamps: true,
+    versionKey: false
+}
+
+);
+
+module.exports = model("Tasks", taskModel)
